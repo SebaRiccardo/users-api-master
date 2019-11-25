d924f7e50355dfb1d71906bd3d15fa89028df4ce
Soathekid <sebaoaki@gmail.com>
2019-11-25 16:33:07 -0300

    minor fixes

--- src/main/java/unsl/controllers/UserController.java
+++ src/main/java/unsl/controllers/UserController.java
@@ -26,7 +26,7 @@ import unsl.utils.RestService;
 
 @RestController
 public class UserController {
-    private static String ipCuentas="localhost";
+    private static String ipCuentas="3.83.42.47";
     private static String port =":8889";
     @Autowired
     UserService userService;
@@ -53,22 +53,20 @@ public class UserController {
     public Object getUser(@PathVariable("userId") Long userId) {
         User user = userService.getUser(userId);
         if ( user == null) {
-            return new ResponseEntity(new ResponseError(400, String.format("Holder with id: %d not found", userId)), HttpStatus.NOT_FOUND);
+            return new ResponseEntity(new ResponseError(404, String.format("El titular con id: %d no existe", userId)), HttpStatus.NOT_FOUND);
         }
         return user;
     }
-
 
     @GetMapping(value = "/users/search")
     @ResponseBody
     public Object searchUser(@RequestParam("dni") Long dni) {
         User user = userService.findByDni(dni);
         if ( user == null) {
-            return new ResponseEntity(new ResponseError(404, String.format("Holder with dni: %d not found", dni)), HttpStatus.NOT_FOUND);
+            return new ResponseEntity(new ResponseError(404, String.format("El titular con dni: %d no existe", dni)), HttpStatus.NOT_FOUND);
         }
         return user;
     }
-
 
     @PostMapping(value = "/users")
     @ResponseStatus(HttpStatus.CREATED)
@@ -78,19 +76,16 @@ public class UserController {
           user.setStatus(User.Status.ACTIVO);
            return userService.saveUser(user);
         }else{
-           return new ResponseEntity(new ResponseError(400, String.format("Holder data is missing ")), HttpStatus.BAD_REQUEST);    
+           return new ResponseEntity(new ResponseError(400, String.format("Informacion del titular faltante")), HttpStatus.BAD_REQUEST);    
         }   
     }
-
 
     @PutMapping(value = "/users/{userId}")
     @ResponseBody
     public Object updateUser(@PathVariable("userId") long id_user, @RequestBody User user) {
-        
         User currentUser = userService.getUser(id_user);
-       
         if (currentUser == null) {
-            return new ResponseEntity(new ResponseError(404, String.format("Holder with id: %d not found", id_user)), HttpStatus.NOT_FOUND);
+            return new ResponseEntity(new ResponseError(404, String.format("El titular con id: %d no existe", id_user)), HttpStatus.NOT_FOUND);
         }
         //controla si los datos no son null
         if(user.getFirstName()==null && user.getLastName()==null ){
@@ -111,22 +106,24 @@ public class UserController {
         return userService.saveUser(currentUser);
     }
 
-    
     @DeleteMapping(value = "/users/{userId}")
     @ResponseBody
     public Object deleteUser(@PathVariable("userId") Long id) throws Exception {
         User res = userService.deleteUser(id);
         if(res == null){
-            return new ResponseEntity(new ResponseError(404,String.format("Holder with id: %d not found", id)), HttpStatus.NOT_FOUND);
+           return new ResponseEntity(new ResponseError(404,String.format("El titular con id: %d no existe", id)), HttpStatus.NOT_FOUND);
         }
-         UserAccounts allAccounts = restService.getAccounts(String.format("http://"+ipCuentas+port+"/accounts/search?holder=%d",res.getId()));
-         for(Account deletedAccount: allAccounts.getUserAccounts()){
-           
+        UserAccounts allAccounts = restService.getAccounts(String.format("http://"+ipCuentas+port+"/accounts/search?holder=%d",res.getId()));
+        for(Account deletedAccount: allAccounts.getUserAccounts()){
+           if(deletedAccount.getAccount_balance().compareTo(new BigDecimal(0))>0){
+                return new ResponseEntity(new ResponseError(400, String
+                .format("La cuenta con id: %d debe tener balance 0 (cero) antes de eliminar el titular de la misma",deletedAccount.getId())), HttpStatus.BAD_REQUEST);
+            }
+            /* aca le paso por url el ?_method=patch porque despues uso postForObject para evitar el error del patchforobject*/
             deletedAccount.setStatus(Account.Status.BAJA);
-
             restService.updateAccountStatus(String.format("http://"+ipCuentas+port+"/accounts/%d?_method=patch",deletedAccount.getId()), deletedAccount);
          }
-        return new ResponseEntity(204,HttpStatus.NO_CONTENT);
+         return new ResponseEntity(204,HttpStatus.NO_CONTENT);
     }
 }
 
