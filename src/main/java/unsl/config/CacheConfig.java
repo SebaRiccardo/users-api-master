12b17108ccb1ace1bae6841cd0bc3b0142a18246
Soathekid <sebaoaki@gmail.com>
2020-06-15 23:00:52 -0300

    parcheado la dependencia

--- src/main/java/unsl/config/CacheConfig.java
+++ src/main/java/unsl/config/CacheConfig.java
@@ -16,13 +16,12 @@ import java.util.concurrent.TimeUnit;
 public class CacheConfig {
 
     public static final String user_CACHE = "users";
-    @Value("${cache.users.maximum.size:2}")
-    private int usersMaxSize;
+    //@Value("${cache.users.maximum.size:2}")
+    //private int usersMaxSize;
 
     @Bean
     public CacheManager cacheManager() {
         SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
-       
        
         simpleCacheManager.setCaches(Arrays.asList(buildUsersCache()));
        
@@ -33,8 +32,8 @@ public class CacheConfig {
     private GuavaCache buildUsersCache() {
         return new GuavaCache(user_CACHE, CacheBuilder
                 .newBuilder()
-                
-                .maximumSize(usersMaxSize)
+                .expireAfterAccess(23,TimeUnit.HOURS)
+                .expireAfterWrite(167, TimeUnit.HOURS)
                 .build(),
                 true);
     }
--- src/main/java/unsl/utils/RestService.java
+++ src/main/java/unsl/utils/RestService.java
@@ -1,8 +1,6 @@
 package unsl.utils;
 
-import java.util.List;
 
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
@@ -16,14 +14,14 @@ public class RestService {
      * @param url
      * @return
      * @throws Exception
-     */
+    */
     public UserAccounts getAccounts(String url) throws Exception {
         RestTemplate restTemplate = new RestTemplate();
         UserAccounts accounts; 
         try {
             accounts = restTemplate.getForObject(url,UserAccounts.class);
         }  catch (Exception e){
-            throw new Exception( buildMessageError(e));
+            throw new Exception(buildMessageError(e));
         }
         return accounts;
     }
@@ -32,16 +30,15 @@ public class RestService {
      * @return
      * @throws Exception
      */
-    public Account updateAccountStatus(String url,Account updatedAccount) throws Exception{
-             
+    public Account updateAccountStatus(String url,Account updatedAccount) throws Exception{     
         RestTemplate restTemplate = new RestTemplate();
         Account account;
         try {
          account = restTemplate.postForObject(url,updatedAccount,Account.class);
         }  catch (Exception e){
         throw new Exception( buildMessageError(e));
-       }
-        return account;
+        }
+       return account;
     }
    
     private String buildMessageError(Exception e) {
